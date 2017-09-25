240665b6698ceccea3e15f9846b6dd4fcfc6166a
MrKeithBryant <kabryant@cinci.rr.com>
2017-09-25 18:19:12 -0400

    pic

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,16 +3,16 @@ import React from 'react';
 export default class Carousel extends React.Component {
   render () {
     return (
-      <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img class="d-block w-100" src="AllStarVans3.jpeg" alt="First slide"/>
+      <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img id='Caro1' className="d-block w-100" src="..." alt="Vans"/>
           </div>
-          <div class="carousel-item">
-            <img class="d-block w-100" src="..." alt="Second slide"/>
+          <div className="carousel-item">
+            <img className="d-block w-100" src="..." alt="Office"/>
           </div>
-          <div class="carousel-item">
-            <img class="d-block w-100" src="..." alt="Third slide"/>
+          <div className="carousel-item">
+            <img className="d-block w-100" src="..." alt="Apartments"/>
           </div>
         </div>
       </div>
